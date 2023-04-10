0ffea75e71a65216ff0ed23f3a00c64d449ad422
el21nb <129977184+el21nb@users.noreply.github.com>
2023-04-10 17:55:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,9 +25,10 @@ GameEngine game;
 ///////////// prototypes ///////////////
 void init();
 void render();
+void introlevel();
 void level1();
-//void welcome();
-//void game_over();
+void welcome();
+void game_over();
 ////////////////////////////////////////
 
 
@@ -40,7 +41,8 @@ int main() {
     int fps = 10;
     thread_sleep_for(1000/fps);  // and wait for one frame period - millseconds
     //int lives = 4;   // display lives on LEDs
-    level1();      
+    introlevel();
+    //level1();      
    // game_over();
    
 }
@@ -57,12 +59,16 @@ void render() {  // clear screen, re-draw and refresh
     game.draw(lcd);
     lcd.refresh();
 }
+/*
 void level1(){
+    welcome();
+    game._level1_done=0;
     game.player_init(0,32,RIGHT1,RIGHT); //void player_init(int player_x,int player_y,SKIN player_skin,PLAYER_DIRECTION player_direction);
     game.floors_init(0,28,70,
                      20,45,60,
                      20,11,65);//(int floor1_x, int floor1_y, int floor1_width, int floor2_x, int floor2_y, int floor2_width, int floor3_x, int floor3_y, int floor3_width)
     game.ladders_init(70,29,15,11,90,90);
+    game.doors_init(0,32,0,LEFT1,69,0,1,RIGHT1);
     for(int i=0;i<10000;i++){
    // while (lives > 0) {  // keep looping while lives remain
         // read the joystick input and store in a struct
@@ -71,12 +77,62 @@ void level1(){
         game.update(input);
         render();                     // draw frame on screen
         ThisThread::sleep_for(300ms);
+        if(game._level1_done){
+            game_over();
+        }
+    }
+}*/
+
+void introlevel(){
+    welcome();
+    game._level_done=0;
+    game.player_init(0,32,RIGHT1,RIGHT); //void player_init(int player_x,int player_y,SKIN player_skin,PLAYER_DIRECTION player_direction);
+    game.floors_init(30,32,40,
+                     0,26,20,
+                     50,17,65);//(int floor1_x, int floor1_y, int floor1_width, int floor2_x, int floor2_y, int floor2_width, int floor3_x, int floor3_y, int floor3_width)
+    game.ladders_init(28,32,50,17,90,90);
+    game.doors_init(0,32,0,LEFT1,69,0,1,RIGHT1);
+    for(int i=0;i<10000;i++){
+   // while (lives > 0) {  // keep looping while lives remain
+        // read the joystick input and store in a struct
+        UserInput input = {joystick.get_direction(),joystick.get_mag()};
+        //lives = pong.update(input);   // update the game engine based on input    
+        game.update(input);
+        render();
+        if(game._level_done){
+            game_over();
+        }                   // draw frame on screen
+        ThisThread::sleep_for(300ms);
+        
+    }
+};
+
+void level1(){
+    welcome();
+    game._level_done=0;
+    game.player_init(0,32,RIGHT1,RIGHT); //void player_init(int player_x,int player_y,SKIN player_skin,PLAYER_DIRECTION player_direction);
+    game.floors_init(0,28,70,
+                     20,45,60,
+                     20,11,65);//(int floor1_x, int floor1_y, int floor1_width, int floor2_x, int floor2_y, int floor2_width, int floor3_x, int floor3_y, int floor3_width)
+    game.ladders_init(70,29,15,11,90,90);
+    game.doors_init(0,32,0,LEFT1,69,0,1,RIGHT1);
+    for(int i=0;i<10000;i++){
+   // while (lives > 0) {  // keep looping while lives remain
+        // read the joystick input and store in a struct
+        UserInput input = {joystick.get_direction(),joystick.get_mag()};
+        //lives = pong.update(input);   // update the game engine based on input    
+        game.update(input);
+        render();                     // draw frame on screen
+        ThisThread::sleep_for(300ms);
+        if(game._level_done){
+            ThisThread::sleep_for(300ms);
+            game_over();
+        }
     }
 }
 
-
-/*void welcome() { // splash screen
-    lcd.printString("     Pong!    ",0,1);  
+void welcome() { // splash screen
+    lcd.printString("          ",0,1);  
     lcd.printString("Press Nucleo",0,3);
     lcd.printString("Blue button",0,4);
     lcd.refresh();
@@ -88,10 +144,10 @@ void level1(){
          ThisThread::sleep_for(100ms);
      
         }
-}*/
+}
 
 
-/*void game_over() { // splash screen
+void game_over() { // splash screen
     while (1) {
         lcd.clear();
         lcd.printString("  Game Over ",0,2);  
@@ -101,4 +157,5 @@ void level1(){
         lcd.refresh();
         ThisThread::sleep_for(250ms);
     }
-}*/
+}
+
