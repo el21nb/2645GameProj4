dcd1077cc399bec7ef96e12a9b93d64c5a2373b9
el21nb <129977184+el21nb@users.noreply.github.com>
2023-05-10 23:21:53 +0100

    Update Dragon.cpp

--- lib/Dragon.cpp
+++ lib/Dragon.cpp
@@ -3,7 +3,7 @@
 Dragon::Dragon() { }
 
 
-void Dragon::init(int x, int y) {
+void Dragon::init(int x, int y) { //initialise dragon
     printf("Dragon: Init\n");
     _skin = LEFT1;
     _x = x;
@@ -12,27 +12,26 @@ void Dragon::init(int x, int y) {
 }
 
 void Dragon::draw(N5110 &lcd) {
-    printf("Dragon: Draw\n");
     if(_skin==LEFT1){
-    lcd.drawSprite(_x,_y,21,32,(int *)_dragon_L1);
+    lcd.drawSprite(_x,_y,21,32,(int *)_dragon_L1); //draw dragon depending on _skin
     }
     else if(_skin==LEFT2){
-    lcd.drawSprite(_x,_y,21,32,(int *)_dragon_L2);
+    lcd.drawSprite(_x,_y,21,32,(int *)_dragon_L2); 
     }
-    lcd.drawRect(_fire_x,_y+14,2,2,FILL_BLACK);
+    lcd.drawRect(_fire_x,_y+14,2,2,FILL_BLACK); //draw fireball
 }
 
 void Dragon::update(){
-    _fire_x-=3;
+    _fire_x-=3; //update fireball position
     if(_fire_x<=0){
         _fire_x=_x;
     }
-    if(_skin==LEFT1){
+    if(_skin==LEFT1){ //update skin (flaps wings)
         _skin=LEFT2;
     }
     else(_skin=LEFT1);
 }
-
+//accessors:
 
 Position2D Dragon::get_pos(){ return {_x,_y}; }
 
@@ -44,6 +43,10 @@ int Dragon::get_fire_x(){return _fire_x;}
 
 void Dragon::set_x(int x){_x=x;}
 void Dragon::set_y(int y){_y=y;}
+
+
+   
+
 
 
    
