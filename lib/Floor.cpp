41eaa89c87003ed74327e30b3b727d15e31828a5
el21nb <129977184+el21nb@users.noreply.github.com>
2023-05-10 23:23:48 +0100

    Update Floor.cpp

--- lib/Floor.cpp
+++ lib/Floor.cpp
@@ -1,12 +1,10 @@
-
 #include "Floor.h"
 
 
 Floor::Floor() {}
 
 
-void Floor::init(int x, int y, int width){
-printf("Floor: Init\n");
+void Floor::init(int x, int y, int width){ //initalise floor
 _x = x;
 _y = y;
 _width = width;
@@ -14,11 +12,11 @@ _width = width;
 
 
 void Floor::draw(N5110 &lcd) {
-    printf("Floor:Draw\n");
-    lcd.drawRect(_x,_y,_width,1,FILL_BLACK);    
+    lcd.drawRect(_x,_y,_width,1,FILL_BLACK);    //draw floor
 }
 
-
+//accessors:
 int Floor::get_y() {return _y;}
 int Floor::get_x() {return _x;}
 int Floor::get_width() {return _width;}
+
